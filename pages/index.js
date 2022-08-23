94735a0c512826ace5844a6cbbcb9d5f38aecaad
lachlan <birdboy307@gmail.com>
2022-08-23 22:23:33 +1000

    initial

--- pages/index.js
+++ pages/index.js
@@ -25,16 +25,3 @@ export default function Home({ prices }) {
     </>
   )
 }
-
-export async function getServerSideProps() {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2020-08-27"
-  });
-
-  const prices = await stripe.prices.list({
-    active: true,
-    expand: ["data.product"],
-  })
-
-  return {props: {prices: prices.data } };
-}

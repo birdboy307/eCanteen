f6f6d0e1e35f736320c005c31e5d9363c4e50d39
birdboy307 <75409790+birdboy307@users.noreply.github.com>
2022-08-25 09:02:24 +1000

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -1,8 +1,6 @@
-import { useRouter } from "next/router"
+import Link from 'next/link'
 
 export default function success() {
-    const router = useRouter()
-
     return (
         <div className='flex justify-center bg-gray-50 h-screen items-center text-center'>
             <div className="grid grid-rows-3 gap-2">
@@ -11,7 +9,9 @@ export default function success() {
                     <p className="mb-2">Please pickup the order from the canteen at either recess or lunch.</p>
                     <p>To pickup the order tell the canteen staff that you have a online order.</p> 
                 </div>
-                <button onClick={() => router.push('/')} className="border-4 font-medium rounded-lg mt-8 py-2.5 bg-white ease-in-out duration-300 hover:bg-gray-300">Return to home</button>
+                <Link href='/'>
+                    <a className="border-4 font-medium rounded-lg mt-8 py-2.5 bg-white ease-in-out duration-300 hover:bg-gray-300">Return to home</a>
+                </Link>
             </div>
         </div>
     )

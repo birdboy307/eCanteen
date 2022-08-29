590b5aa62ed7fe96af7d50a3908ac0fb33d64222
birdboy307 <75409790+birdboy307@users.noreply.github.com>
2022-08-29 11:54:36 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js}",
+    "./components/**/*.{js}",
   ],
   theme: {
     extend: {
-      colors: {
-        'charcoal': '#222222',
-        'creamish': '#EAF2F4',
-        'pastel': '#7BEA71',
-        'pastel-hover': '#4FC844',
-      },
       fontFamily: {
         heading: ['Dosis', 'sans-serif'],
       },
